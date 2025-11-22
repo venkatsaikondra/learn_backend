05beab629f7ee349ca45cfa470870a482f8fc12e
Venkat Sai Kondra <venkatsaikondra67@gmail.com>
2025-11-23 01:20:55 +0530

    added mongodb for registration

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const morgan = require('morgan');
 const userModel=require('./models/user');
+const dbConnection=require('./config/db');
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +15,14 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index');
 });
-
+app.get('/register',(req,res)=>{
+    res.render('register');
+});
+app.post('/register',async (req,res)=>{
+    const {username,email,password}=req.body;
+    const newUser=await userModel.create({username:username,email:email,password:password});
+    res.send(newUser);
+});
 app.get('/about', (req, res) => {
     res.send('About Us Page');
 });
--- config/db.js
+++ config/db.js
@@ -1,1 +1,5 @@
 const mongoose=require('mongoose');
+const connection=mongoose.connect('mongodb://localhost:27017/learning').then(()=>{
+    console.log('Connected to MongoDB');
+})
+module.exports=connection;
